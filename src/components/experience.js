af4f0bb64ccf1507d929789a388ba90351de9176
thluiz <contato@thluiz.com>
2021-02-08 06:30:43 -0300

    Adding Achiviments section

--- src/components/experience.js
+++ src/components/experience.js
@@ -12,6 +12,12 @@ const Experience = ({ data }) => (
           </h3>
           { item.description && <p className="pt-2" dangerouslySetInnerHTML={{__html: item.description }} ></p> }
           { item.descriptions && item.descriptions.map(description => <p className="pt-2" dangerouslySetInnerHTML={{__html: description }} ></p>) }
+          { item.achievements && <section>
+            <h2 className="text-secondary-500 p-0">{item.achievements.title}</h2>
+            <ul class="list-disc ml-5 p-0">
+              { item.achievements.list.map(achievement => <li className="pb-1" dangerouslySetInnerHTML={{__html: achievement }} ></li>) }
+            </ul>
+          </section>}
         </article>
       ))}
   </section>
